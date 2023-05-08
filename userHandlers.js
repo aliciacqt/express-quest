aa5bbd0c3e3f215be1c75f28e4238a8b3b2a34cc
alicia coquerant <alicia.coquerant@gmail.com>
2023-05-08 15:29:34 +0200

    created a POST route and request for movies and for users

--- userHandlers.js
+++ userHandlers.js
@@ -18,10 +18,9 @@ const getUserById = (req, res) => {
 
   database
     .query("select * from users where id = ?", [id])
-    .then((result) => {
-      const user = result[0];
-      if (user != null) {
-        res.status(200).json(user);
+    .then(([users]) => {
+      if (users[0] != null) {
+        res.status(200).json(users[0]);
       } else {
         res.status(404).send("Not Found");
       }
@@ -32,7 +31,25 @@ const getUserById = (req, res) => {
     });
 };
 
+const postUser = (req, res) => {
+  const { firstname, lastname, email, city, language } = req.body;
+
+  database
+    .query(
+      "INSERT INTO users (firstname, lastname, email, city, language) VALUES (?, ?, ?, ?, ?)",
+      [firstname, lastname, email, city, language]
+    )
+    .then(([result]) => {
+      res.location(`api/users/${result.insertId}`).sendStatus(201);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error saving the user");
+    });
+};
+
 module.exports = {
   getUsers,
   getUserById,
+  postUser,
 };
